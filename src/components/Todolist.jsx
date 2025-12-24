71010dbb2c31e83c1884b7e2b9ccad40e1e0a481
Desmond4898 <desmondpauldope@gmail.com>
2025-12-24 14:13:46 -0800

    Add files via upload

---
+++ src/components/Todolist.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+function Todolist() {
+    const [tasks, setTasks] = useState(["Do an exercise", "Go to Church", "Go on a Date"]);
+    const [newTask, setNewTAsk] = useState("")
+
+    function handleInput(e){
+        setNewTAsk(e.target.value)
+    }
+
+    function addTask() {
+        if(newTask.trim() !== "")
+            setTasks(t => [...t, newTask]);
+        setNewTAsk("");
+
+    }
+
+    function deleteTask(index) {
+        const updateTasks = tasks.filter((_, i) => i !==index);
+        setTasks(updateTasks)
+
+    }
+
+    return(
+        <div className="to-do">
+            <h1>My Todo List</h1>
+            <div>
+                <input type="text" placeholder="enter a task"  value={newTask} onChange={handleInput}/>
+                <button className= "add-button" onClick={addTask}>Add</button>
+            </div>
+            <ol>
+                {tasks.map((task, index) =>
+                <li key={index}>
+                    <span className="text">{task}</span>
+                    <button className="delete-button" onClick={() => deleteTask(index)}>Delete</button>
+                </li>)}
+            </ol>
+        </div>
+    )
+}
+
+export default Todolist
